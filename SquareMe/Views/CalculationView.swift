6cc71439b1c271a15a4ac483dd276e9b2a668173
Alistair Fraser <afraser@lcs.on.ca>
2023-02-02 14:23:25 -0500

    We have written a computed property to get the input given as an optional Double.

--- SquareMe/Views/CalculationView.swift
+++ SquareMe/Views/CalculationView.swift
@@ -8,24 +8,37 @@
 import SwiftUI
 struct CalculationView: View {
     //MARK: Stored Properties
-    //Keeps the input gicen by user
+    //Keeps the input given by user
+    @State var inputGiven = ""
     
     //MARK: Computed Propertie
-    @State var inputGiven = ""
-    var body: some View {
-        VStack {
-            TextField("5.5", text: $inputGiven)
-                .font(.largeTitle)
-                .padding()
+    //Convert the input givern into an optonal Double
+    var inputGivenAsOptionalDouble: Double? {
+        //Create optional Double
+        guard let inputGivenAsDouble = Double(inputGiven) else {
+            //User gave us bad input cannot convert to Double
+            return nil
         }
-        .navigationTitle("Square Me")
+    }
+    
+    //We have a Double so return it
+    return inputGivenAsDouble
+    
+    }
+    var body: some View {
+        NavigationView() {
+            VStack {
+                TextField("5.5", text: $inputGiven)
+                    .font(.largeTitle)
+                    .padding()
+            }
+            .navigationTitle("Square Me")
+        }
     }
 }
 
 struct CalculationView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
             CalculationView()
-        }
     }
 }
