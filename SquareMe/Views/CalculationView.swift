ec5a1cc487f5af64e7551f8301751fdb9ab4d9e6
Alistair Fraser <afraser@lcs.on.ca>
2023-02-02 14:07:51 -0500

    Made a view called CalculationView

---
+++ SquareMe/Views/CalculationView.swift
@@ -0,0 +1,23 @@
+//
+//  CalculationView.swift
+//  SquareMe
+//
+//  Created by Alistair Fraser on 2023-02-02.
+//
+
+import SwiftUI
+//MARK: Stored Properties
+
+struct CalculationView: View {
+    
+//MARK: Computed Properties
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalculationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculationView()
+    }
+}
